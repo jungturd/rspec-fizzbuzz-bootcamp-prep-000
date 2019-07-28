ffc702ff33b27ba68d070438173f46159163a471
Jeemin Shim <jeemin.shim@gmail.com>
2019-07-28 07:46:31 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-require_relative './spec_helper.rb'
+def fizzbuzz
+  if fizzbuss % 3 == 0
+  puts "Fizz"
+end
 
