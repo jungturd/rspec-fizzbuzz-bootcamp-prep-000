9d6ae83ba91e1c8ef5125494d474449efd107340
Jeemin Shim <jeemin.shim@gmail.com>
2019-07-28 08:16:36 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,13 +5,8 @@ require_relative './spec/fizzbuzz_spec.rb'
 def fizzbuzz (int)
   if int % 3 == 0
     "Fizz"
-if int % 5 == 0
-    "Buzz"
-if int % 15 == 0
-    "FizzBuzz"
-  end
-  end
-  end
+    if int % 5 == 0
+      "Buzz"
 end
 
 puts fizzbuzz(3)
