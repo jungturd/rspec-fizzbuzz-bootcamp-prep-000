83de173ee706430562c3415931c99e084f58bb50
Jeemin Shim <jeemin.shim@gmail.com>
2019-07-28 07:58:25 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,8 +2,13 @@
 # See README.md for instructions on how to do this
 def fizzbuzz (int)
   if int % 3 == 0
-  "Fizz"
+    "Fizz"
   end
+  if int % 5 == 0
+    "Buzz"
+  end
+  if int % 3 && 5 == 0
+    "FizzBuzz"
 end
 
 fizzbuzz(3)
