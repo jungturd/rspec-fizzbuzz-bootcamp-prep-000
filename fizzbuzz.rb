86401a9ae6626d818ad9fb9d19e9bc39ce39eb46
Jeemin Shim <jeemin.shim@gmail.com>
2019-07-28 07:47:16 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz
-  if fizzbuss % 3 == 0
-  puts "Fizz"
+def fizzbuzz (num)
+  if num % 3 == 0
+    puts "Fizz"
+  end
 end
+
+fizzbuzz(3)
+fizzbuzz(5)
 
