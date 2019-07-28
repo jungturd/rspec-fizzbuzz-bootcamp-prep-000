0b6912c168b402dedd8007da2bcbbc214c7d29fd
Jeemin Shim <jeemin.shim@gmail.com>
2019-07-28 08:14:43 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 require_relative './spec/fizzbuzz_spec.rb'
-
 def fizzbuzz (int)
   if int % 3 == 0
     "Fizz"
-elsif int % 5 == 0
+else int % 5 == 0
     "Buzz"
-elsif int % 3 && 5 == 0
+    end
+if int % 15 == 0
     "FizzBuzz"
   end
 end
